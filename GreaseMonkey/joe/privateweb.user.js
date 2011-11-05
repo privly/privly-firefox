b191817ff6b490b247c995162da6d361c10c74ce
Joe Selman <selman.joe@gmail.com>
2011-11-05 15:37:06 -0700

    more basic changes.  now pulls in front page content.  request is synchronous, should be async

--- GreaseMonkey/joe/privateweb.user.js
+++ GreaseMonkey/joe/privateweb.user.js
@@ -10,6 +10,23 @@
     var toFind = "http://50.57.176.145";
     var bodytext = document.body.textContent;
     if (bodytext.indexOf(toFind) != -1) {
-        alert("working!");
+      // do a more complicated check for links/pre/p/span content
+      var as = document.body.getElementsByTagName('a');
+      for (var i = 0; i < as.length; i++) {
+		    if (as[i].href.indexOf(toFind) == 0) {
+          // below only guaranteed to work on firefox
+          var request = new XMLHttpRequest();
+          // hardcoded URL below, also asynchronous
+          request.open('GET', "http://50.57.176.145", false);
+          try {
+            request.send();
+            if (request.status == 200) {
+              as[i].innerHTML = request.responseText;
+            }
+          } catch(e) {
+            throw(e);
+          }
+        }
+      }	
     }    
 })();
