8006c7e971481fdd1080c38ab76280c2af1367c2
Sean McGregor <smcgregor08@cmc.edu>
2011-12-06 16:07:44 -0800

    added devise for authentication

---
+++ WebApp/babel/app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, 
+         :validatable, :token_authenticatable, 
+         :confirmable, :lockable, :timeoutable,
+         :omniauthable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :email, :password, :password_confirmation, :remember_me
+end
--- WebApp/babel/config/environments/production.rb
+++ WebApp/babel/config/environments/production.rb
@@ -57,4 +57,6 @@ Privly::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  
+  config.action_mailer.default_url_options = { :host => 'priv.ly:80' }
 end
