1eedae65bdd7e35e89c277686e9f0d2c8ad3c1b1
Sean McGregor <smcgregor08@cmc.edu>
2011-12-15 01:09:30 -0800

    migration required. added users to posts and updated layouts

--- WebApp/babel/app/models/user.rb
+++ WebApp/babel/app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   
-  has_many :authentications
+  has_many :authentications, :dependent => :destroy
+  has_many :posts, :dependent => :destroy
   
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
