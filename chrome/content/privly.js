9f383c66871f3c9f656d67fed1afb9255cd59e35
Sean McGregor <smcgregor08@cmc.edu>
2012-06-02 13:01:14 -0700

    white space changes, no semantic changes

--- chrome/content/privly.js
+++ chrome/content/privly.js
@@ -5,22 +5,22 @@ The MIT License (MIT)
 
 Copyright (c) Sean McGregor
 
-Permission is hereby granted, free of charge, to any person obtaining a copy 
-of this software and associated documentation files (the "Software"), to deal 
-in the Software without restriction, including without limitation the rights 
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
-copies of the Software, and to permit persons to whom the Software is 
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in 
+The above copyright notice and this permission notice shall be included in
 all copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
-EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
-MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
-IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
-DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, 
-ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 DEALINGS IN THE SOFTWARE.
 
 *******************************************************************************/
@@ -28,6 +28,7 @@ DEALINGS IN THE SOFTWARE.
 
 
 /**
+ *
  * For a high level overview of what this script does, see:
  * http://www.privly.org/content/core-functionality-privlyjs
  *
@@ -50,8 +51,8 @@ DEALINGS IN THE SOFTWARE.
  *
  ****URL PARAMETERS***
  *
- * The script also responds to parameters 
- * and anchors on the URL. 
+ * The script also responds to parameters
+ * and anchors on the URL.
  *
  * burntAfter: specifies a time in seconds in the Unix epoch
  * until the content is likely destroyed on the remote server
@@ -74,7 +75,7 @@ var privly = {
   //These messages are displayed to users. These strings cannot be in this
   //script when we start localization.
   messages: {
-    sleepMode: "Privly is in sleep mode so it can catch up with " + 
+    sleepMode: "Privly is in sleep mode so it can catch up with " +
       "demand. The content may still be viewable by clicking this link",
     passiveModeLink: "Read in Place",
     contentExpired: "The content behind this link likely expired. Click the link to check.",
@@ -90,21 +91,21 @@ var privly = {
   // privly.getUrlVariables(url)["anchor"] is "World"
   getUrlVariables: function(url) {
       var vars = {};
-      if(url.indexOf("#",0) > 0)
+      if (url.indexOf("#",0) > 0)
       {
         var anchor = url.substring(url.indexOf("#",0) + 1, url.length);
         vars["anchor"] = anchor;
         url = url.split("#",1)[0];
       }
       url = url.replace("&amp;", "&")
-      var parts = url.replace(/[?&]+([^=&]+)=([^&]*)/gi, 
+      var parts = url.replace(/[?&]+([^=&]+)=([^&]*)/gi,
         function(m,key,value) {
           vars[key] = value;
       });
       return vars;
   },
   
-  // The Privly RegExp determines which links are eligible for 
+  // The Privly RegExp determines which links are eligible for
   // replacing with their referenced content.
   // This system will need to change so we can move to a white
   // list approach. See: http://www.privly.org/content/why-privly-server
@@ -113,23 +114,23 @@ var privly = {
   //              http://
   //              https://
   //                        DOMAIN/number
-  //                                      
+  //
   //                                  ?any_number_of_parameters=value#anchors
-  privlyReferencesRegex: new RegExp("\\b(https?:\\/\\/){0,1}(" + 
+  privlyReferencesRegex: new RegExp("\\b(https?:\\/\\/){0,1}(" +
     "priv\\.ly\\/posts\\/\\d+|" +
     "dev\\.privly\\.org\\/posts\\/\\d+|" +
     "privly\\.org\\/posts\\/\\d+|" +
     "privly\\.com\\/posts\\/\\d+|" +
     "dev\\.privly\\.com\\/posts\\/\\d+|" +
-    "localhost:3000\\/posts\\/\\d+" + 
+    "localhost:3000\\/posts\\/\\d+" +
     ")(\\b|\\?(\\S)*|#(\\S)*)$","gi"),
-    //the final line matches 
+    //the final line matches
     //end of word OR
     //the parameter string to the end of the word OR
     //the anchor string to the end of the word
   
-  // enum to hold various extension modes and their value. 
-  // extension modes are set through firefox's extension api. 
+  // enum to hold various extension modes and their value.
+  // extension modes are set through firefox's extension api.
   // https://developer.mozilla.org/en/Code_snippets/Preferences
   extensionModeEnum : {
     ACTIVE : 0,
@@ -141,64 +142,64 @@ var privly = {
   //links it encounters
   extensionMode: 0,
   
-  // Takes a domain with an optional http(s) 
+  // Takes a domain with an optional http(s)
   // in front and returns a fully formed domain name
   makeHref: function(domain)
   {
     var hasHTTPRegex = /^((https?)\:\/\/)/i;
-    if(!hasHTTPRegex.test(domain))
+    if (!hasHTTPRegex.test(domain))
         domain = "http://" + domain;
     return domain;
   },
   
   //Make plain text links into anchor elements
-  createLinks: function() 
+  createLinks: function()
   {
       /***********************************************************************
       Inspired by Linkify script:
         http://downloads.mozdev.org/greasemonkey/linkify.user.js
-
+        
       Originally written by Anthony Lieuallen of http://arantius.com/
       Licensed for unlimited modification and redistribution as long as
       this notice is kept intact.
       ************************************************************************/
-
+      
       var excludeParents = ["a", "applet", "button", "code", "form",
-                             "input", "option", "script", "select", "meta", 
+                             "input", "option", "script", "select", "meta",
                              "style", "textarea", "title", "div","span"];
       var excludedParentsString = excludeParents.join(" or parent::");
-      var xpathExpression = ".//text()[not(parent:: " + 
+      var xpathExpression = ".//text()[not(parent:: " +
           excludedParentsString +")]";
-
-      textNodes = document.evaluate(xpathExpression, document.body, null, 
+          
+      textNodes = document.evaluate(xpathExpression, document.body, null,
           XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
-
-      for(var i=0; i < textNodes.snapshotLength; i++){
+          
+      for (var i=0; i < textNodes.snapshotLength; i++){
           item = textNodes.snapshotItem(i);
-
+          
           var itemText = item.nodeValue;
           
           privly.privlyReferencesRegex.lastIndex = 0;
-          if(privly.privlyReferencesRegex.test(itemText)){
-              var span = document.createElement("span");    
+          if (privly.privlyReferencesRegex.test(itemText)){
+              var span = document.createElement("span");
               var lastLastIndex = 0;
               privly.privlyReferencesRegex.lastIndex = 0;
               
-              for(var results = null; 
+              for (var results = null;
                 results = privly.privlyReferencesRegex.exec(itemText); ){
                   var href = results[0];
                   span.appendChild(document.createTextNode(
                     itemText.substring(lastLastIndex, results.index)));
-
+                    
                   var text = (href.indexOf(" ")==0)?href.substring(1):href;
-
+                  
                   var href = privly.makeHref(text);
-
+                  
                   var a = document.createElement("a");
                   a.setAttribute("href", href);
                   a.appendChild(document.createTextNode(
                     text.substring(0,4).toLowerCase() + text.substring(4)));
-                  if(href.indexOf(" ")==0) 
+                  if (href.indexOf(" ") == 0)
                       span.appendChild(document.createTextNode(" "));
                   span.appendChild(a);
                   lastLastIndex = privly.privlyReferencesRegex.lastIndex;
@@ -211,7 +212,7 @@ var privly = {
   },
   
   //Kill default link behaviour on Privly Links
-  makePassive: function(e) 
+  makePassive: function(e)
   {
     //Preventing the default link behavior
     e.cancelBubble = true;
@@ -223,7 +224,7 @@ var privly = {
   //Checks link attributes and text for privly links without the proper href
   //attribute. Twitter and other hosts change links so they can collect
   //click events.
-  correctIndirection: function() 
+  correctIndirection: function()
   {
     var anchors = document.links;
     var i = anchors.length;
@@ -231,11 +232,11 @@ var privly = {
       var a = anchors[i];
       
       privly.privlyReferencesRegex.lastIndex = 0;
-      if(a.href && !privly.privlyReferencesRegex.test(a.href))
+      if (a.href && !privly.privlyReferencesRegex.test(a.href))
       {
         //check if Privly is in the body of the text
         privly.privlyReferencesRegex.lastIndex = 0;
-        if (privly.privlyReferencesRegex.test(a.innerHTML)) {        
+        if (privly.privlyReferencesRegex.test(a.innerHTML)) {
           // If the href is not present or is on a different domain
           privly.privlyReferencesRegex.lastIndex = 0;
           var results = privly.privlyReferencesRegex.exec(a.innerHTML);
@@ -257,13 +258,13 @@ var privly = {
       privly.privlyReferencesRegex.lastIndex = 0;
     }
   },
-
+  
   nextAvailableFrameID: 0,
   
   // Replace an anchor element with its referenced content.
-  replaceLink: function(object) 
+  replaceLink: function(object)
   {
-    if(object.parentNode != null){
+    if (object.parentNode != null){
       var iFrame = document.createElement('iframe');
       iFrame.setAttribute("frameborder","0");
       iFrame.setAttribute("vspace","0");
@@ -273,12 +274,12 @@ var privly = {
       iFrame.setAttribute("marginwidth","0");
       iFrame.setAttribute("marginheight","0");
       iFrame.setAttribute("height","1px");
-      if(object.href.indexOf("?") > 0){
+      if (object.href.indexOf("?") > 0){
         var iframeUrl = object.href.replace("?",".iframe?frame_id="+
           privly.nextAvailableFrameID+"&");
         iFrame.setAttribute("src",iframeUrl);
       }
-      else if(object.href.indexOf("#") > 0)
+      else if (object.href.indexOf("#") > 0)
       {
         var iframeUrl = object.href.replace("#",".iframe?frame_id="+
           privly.nextAvailableFrameID+"#");
@@ -286,13 +287,13 @@ var privly = {
       }
       else
       {
-        iFrame.setAttribute("src",object.href + ".iframe?frame_id=" + 
+        iFrame.setAttribute("src",object.href + ".iframe?frame_id=" +
           privly.nextAvailableFrameID);
       }
       iFrame.setAttribute("id","ifrm"+privly.nextAvailableFrameID);
       iFrame.setAttribute("frameborder","0");
       privly.nextAvailableFrameID++;
-      iFrame.setAttribute("style","width: 100%; height: 32px; " + 
+      iFrame.setAttribute("style","width: 100%; height: 32px; " +
         "overflow: hidden;");
       iFrame.setAttribute("scrolling","no");
       iFrame.setAttribute("overflow","hidden");
@@ -301,7 +302,7 @@ var privly = {
   },
   
   //Process a link according to its parameters and whitelist status
-  //if the link is in active mode and is whitelisted, it will replace 
+  //if the link is in active mode and is whitelisted, it will replace
   //the link with the referenced content. If the link is in passive mode
   //or it is not a whitelisted link, the link will be clickable to replace
   //the content.
@@ -310,38 +311,38 @@ var privly = {
     var exclude = anchorElement.getAttribute("privly-exclude");
     var params = privly.getUrlVariables(anchorElement.href);
     
-    if(!exclude && !params["exclude"]){
+    if (!exclude && !params["exclude"]){
       
       var passive = this.extensionMode == privly.extensionModeEnum.PASSIVE ||
         params["passive"] != null || !whitelist;
-      var burnt = params["burntAfter"] != null && parseInt(params["burntAfter"]) < 
+      var burnt = params["burntAfter"] != null && parseInt(params["burntAfter"]) <
         Date.now()/1000;
-      var active = this.extensionMode == privly.extensionModeEnum.ACTIVE && 
+      var active = this.extensionMode == privly.extensionModeEnum.ACTIVE &&
         whitelist;
-      var sleepMode = this.extensionMode == privly.extensionModeEnum.CLICKTHROUGH && 
+      var sleepMode = this.extensionMode == privly.extensionModeEnum.CLICKTHROUGH &&
         whitelist;
       
-      if(!whitelist){
-        anchorElement.innerHTML = privly.messages.injectableContent + 
+      if (!whitelist){
+        anchorElement.innerHTML = privly.messages.injectableContent +
           privly.messages.passiveModeLink;
         anchorElement.addEventListener("mousedown",privly.makePassive,true);
       }
-      else if(passive){
-        if(params["passiveMessage"] != null)
+      else if (passive){
+        if (params["passiveMessage"] != null)
         {
           var passiveMessage = params["passiveMessage"].replace(/\+/g, " ");
           anchorElement.innerHTML = privly.messages.privlyContent + passiveMessage;
         }
         else
         {
-          anchorElement.innerHTML = privly.messages.privlyContent + 
+          anchorElement.innerHTML = privly.messages.privlyContent +
             privly.messages.passiveModeLink;
         }
         anchorElement.addEventListener("mousedown",privly.makePassive,true);
       }
-      else if(burnt)
+      else if (burnt)
       {
-        if(params["burntMessage"] != null)
+        if (params["burntMessage"] != null)
         {
           var burntMessage = params["burntMessage"].replace(/\+/g, " ");
           anchorElement.innerHTML = privly.messages.burntPrivlyContent + burntMessage;
@@ -353,10 +354,10 @@ var privly = {
         anchorElement.setAttribute('target','_blank');
         anchorElement.addEventListener("mousedown",privly.makePassive,true);
       }
-      else if(active){
+      else if (active){
         this.replaceLink(anchorElement);
       }
-      else if(sleepMode){
+      else if (sleepMode){
         anchorElement.innerHTML = privly.messages.sleepMode;
         anchorElement.setAttribute('target','_blank');
         anchorElement.removeEventListener("mousedown",privly.makePassive,true);
@@ -370,34 +371,34 @@ var privly = {
   replaceLinks: function()
   {
     elements = document.getElementsByTagName("privModeElement");
-    if(elements != null && elements.length != 0){
+    if (elements != null && elements.length != 0){
       this.extensionMode = elements[0].getAttribute('mode');
     }
     var anchors = document.links;
     var i = anchors.length;
-
+    
     while (--i >= 0){
       var a = anchors[i];
       this.privlyReferencesRegex.lastIndex = 0;
-      if(a.href && this.privlyReferencesRegex.test(a.href))
+      if (a.href && this.privlyReferencesRegex.test(a.href))
       {
         privly.processLink(a, true);
       }
-      else if(a.href && a.href.indexOf("#INJECTCONTENT0",0) > 0)
+      else if (a.href && a.href.indexOf("#INJECTCONTENT0",0) > 0)
       {
         privly.processLink(a, false);
       }
     }
   },
-
+  
   //resize the iframe using a posted message
   resizeIframe: function(message){
     
-    if(message.origin !== "https://priv.ly" && 
+    if (message.origin !== "https://priv.ly" &&
       message.origin !== "http://localhost:3000" &&
-      message.origin !== "http://dev.privly.org" && 
-      message.origin !== "http://dev.privly.com" && 
-      message.origin !== "https://privly.org" && 
+      message.origin !== "http://dev.privly.org" &&
+      message.origin !== "http://dev.privly.com" &&
+      message.origin !== "https://privly.org" &&
       message.origin !== "https://privly.com")
       return;
       
@@ -422,7 +423,7 @@ var privly = {
     //otherwise replace all links default behavior
     privly.replaceLinks();
   },
-
+  
   //runs privly once then registers the update listener
   //for dynamic pages
   listeners: function(){
@@ -431,9 +432,9 @@ var privly = {
     //height
     window.addEventListener("message", privly.resizeIframe, false, true);
     
-    //respect the settings of the host page. 
+    //respect the settings of the host page.
     //If the body element has privly-exclude=true
-    if(document.getElementsByTagName("body")[0]
+    if (document.getElementsByTagName("body")[0]
         .getAttribute("privly-exclude")=="true")
     {
       return;
@@ -447,15 +448,15 @@ var privly = {
           privly.run();
         },
         100);
-
+        
       //Everytime the page is updated via javascript, we have to check
       //for new Privly content. This might not be supported on other platforms
       document.addEventListener("DOMNodeInserted", function(event) {
         //we check the page a maximum of two times a second
-        if(privly.runPending)
+        if (privly.runPending)
           return;
         privly.runPending=true;
-
+        
         setTimeout(
           function(){
             privly.runPending=false;
@@ -467,22 +468,21 @@ var privly = {
   },
   
   // cross platform onload event
-  // won't attach anything on IE 
+  // won't attach anything on IE
   // on macintosh systems.
-  addEvent: function(obj, evType, fn){ 
-    if (obj.addEventListener){ 
-      obj.addEventListener(evType, fn, false); 
-      return true; 
+  addEvent: function(obj, evType, fn){
+    if (obj.addEventListener){
+      obj.addEventListener(evType, fn, false);
+      return true;
     }
-    else if (obj.attachEvent){ 
-      var r = obj.attachEvent("on"+evType, fn); 
-      return r; 
+    else if (obj.attachEvent){
+      var r = obj.attachEvent("on"+evType, fn);
+      return r;
     }
-    else { 
-      return false; 
-    } 
+    else {
+      return false;
+    }
   }
 }
 
-privly.addEvent(window, 'load', privly.listeners); 
-
+privly.addEvent(window, 'load', privly.listeners);
